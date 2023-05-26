ce323d3aa1ec5a26b09f4dfe8f9e85e05c136b02
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-05-26 14:59:05 -0400

    implemted rendering logic for food page

--- mercado/src/pages/Food.js
+++ mercado/src/pages/Food.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Food = () => {
+  const [foods, setFoods] = useState([])
+
+  useEffect(() => {
+    const getFoods = async () => {
+      const response = await axios.get('http://localhost:3001/food')
+      setFoods(response.data)
+    }
+    getFoods()
+  }, [])
+
   return (
     <div>
-      <h1>Food</h1>
+      {foods
+        ? foods.map((food) => (
+            <div>
+              <h2>Description: {food.description}</h2>
+              <h1>Price: ${food.price}</h1>
+              <img src={food.picture1} width="200" />
+            </div>
+          ))
+        : null}
     </div>
   )
 }
