76cfd28e05218474b622ff25b67e5548de04856f
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-05-26 15:08:33 -0400

    rendering logic for produce page

--- mercado/src/pages/Produce.js
+++ mercado/src/pages/Produce.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Produce = () => {
+  const [produce, setProduce] = useState([])
+
+  useEffect(() => {
+    const getProduce = async () => {
+      const response = await axios.get('http://localhost:3001/produce')
+      setProduce(response.data)
+    }
+    getProduce()
+  }, [])
+
   return (
     <div>
-      <h1>Produce</h1>
+      {produce
+        ? produce.map((item) => (
+            <div>
+              <h2>description: {item.description}</h2>
+              <h1>Price: ${item.price}</h1>
+              <img src={item.picture1} width="300" />
+            </div>
+          ))
+        : null}
     </div>
   )
 }
