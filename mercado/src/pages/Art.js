0643a71910fd3cb692a4b304bd1b1b4e566e536c
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-05-26 14:46:10 -0400

    implemented rendering logic for data on articles/community page

--- mercado/src/pages/Art.js
+++ mercado/src/pages/Art.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import React from 'react'
 
 const Art = () => {
   const [artWork, setArtWork] = useState([])
--- mercado/src/pages/Articles.js
+++ mercado/src/pages/Articles.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 const Articles = () => {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axios.get('http://localhost:3001/community')
+      setArticles(response.data)
+    }
+    getArticles()
+  }, [])
+
   return (
     <div>
-      <h1>Articles</h1>
+      {articles
+        ? articles.map((article) => (
+            <div>
+              <h2>description: {article.description}</h2>
+              <h3>Read about it : {article.link}</h3>
+              <img src={article.picture1} width="600" />
+            </div>
+          ))
+        : null}
     </div>
   )
 }
