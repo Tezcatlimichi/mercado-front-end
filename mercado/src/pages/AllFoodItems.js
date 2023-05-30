55d6b4edfa3661d13ceef49e9c85691cbcbb42d4
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-05-30 15:07:08 -0400

    rendering logic for all food item page

--- mercado/src/pages/AllFoodItems.js
+++ mercado/src/pages/AllFoodItems.js
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const AllFoodItems = () => {
+  const [produce, setProduce] = useState([])
+  const [foods, setFoods] = useState([])
+
+  useEffect(() => {
+    const getProduce = async () => {
+      const response = await axios.get('http://localhost:3001/produce')
+      setProduce(response.data)
+    }
+    getProduce()
+    const getFood = async () => {
+      const response = await axios.get('http://localhost:3001/food')
+      setFoods(response.data)
+    }
+    getFood()
+  }, [])
+
   return (
     <div>
-      <h1>Food ready to eat</h1>
+      <div>
+        {produce
+          ? produce.map((item) => (
+              <div>
+                <img src={item.picture1} width="300" />
+              </div>
+            ))
+          : null}
+      </div>
+      <div>
+        {foods
+          ? foods.map((food) => (
+              <div>
+                <img src={food.picture1} width="200" />
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   )
 }
