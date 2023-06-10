faabe81ff46a4215044f34954a01ef3082e24f97
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-06-10 17:59:50 -0400

    render logic for selected Article with useParams

--- mercado/src/components/OneArticle.jsx
+++ mercado/src/components/OneArticle.jsx
@@ -1,7 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 const OneArticle = ()=>{
+let {id} = useParams()
+
+const [selectedArticle, setSelectedArticle ] = useState("")
+
+useEffect(()=>{
+  const getArticle = async () =>{
+    const response = await axios.get(`http://localhost:3001/community/${id}`)
+    setSelectedArticle(response.data)
+  }
+  getArticle()
+},[])
+
   return(
     <div>
-      <h1>Single Article</h1>
+      <h1>{selectedArticle.description}</h1>
+      <img src={selectedArticle.picture1} width="600" />
     </div>
   )
 }
