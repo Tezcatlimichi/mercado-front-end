950bcdcf704d9f0c3e2b404c0bf93d7e40cef9da
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-06-13 19:43:22 -0400

    render logic for selected service with useParams

--- mercado/src/components/OneService.jsx
+++ mercado/src/components/OneService.jsx
@@ -1,7 +1,29 @@
+import React from 'react'
+import axios from 'axios'
+import  { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+
+
 const OneService =()=>{
+  let {id} = useParams()
+
+  const [selectedService, setSelectedService] = useState("")
+
+  useEffect(()=>{
+    const getService = async()=>{
+      const response = await axios.get(`http://localhost:3001/services/${id}`)
+      setSelectedService(response.data)
+    }
+    getService()
+  },[])
+
+
+
   return(
     <div>
-      <h1>Single Service</h1>
+      <h1>{selectedService.description}</h1>
+      <img src={selectedService.picture1} width="600"/>
     </div>
   )
 }
