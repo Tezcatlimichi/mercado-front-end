bcca98f6b739d383eb70b3134d967a59f35b30db
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-06-13 19:49:13 -0400

    render logic for selected art with useParams

--- mercado/src/components/PieceOfArt.jsx
+++ mercado/src/components/PieceOfArt.jsx
@@ -1,7 +1,27 @@
+import React from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 const PieceOfArt = ()=>{
+let {id} = useParams()
+
+const [selectedArt, setSelectedArt] = useState("")
+
+useEffect(()=>{
+  const getArtPiece = async()=>{
+    const response = await axios.get(`http://localhost:3001/art/${id}`)
+    setSelectedArt(response.data)
+  }
+  getArtPiece()
+},[])
+
+
+
+
   return(
     <div>
-      <h1>SIngle Art Piece</h1>
+      <h1>{selectedArt.description}</h1>
+      <img src={selectedArt.picture1} width="600" />
     </div>
   )
 }
