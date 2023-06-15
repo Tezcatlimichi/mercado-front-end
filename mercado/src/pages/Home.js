e2011f6fa53c023cf9f141329f14e1ca4db69cdf
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-06-14 22:01:31 -0400

    render logic for home page

--- mercado/src/pages/Home.js
+++ mercado/src/pages/Home.js
@@ -1,7 +1,74 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 const Home = () => {
+  const [articles, setArticles] = useState([])
+  const [markets, setMarkets] = useState([])
+  const [art, setArt] = useState([])
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axios.get(`http://localhost:3001/community`)
+      setArticles(response.data)
+    }
+    getArticles()
+    const getMarkets = async () => {
+      const response = await axios.get(`http://localhost:3001/markets`)
+      setMarkets(response.data)
+    }
+    getMarkets()
+    const getArt = async () => {
+      const response = await axios.get(`http://localhost:3001/art`)
+      setArt(response.data)
+    }
+    getArt()
+    const getServices = async () => {
+      const response = await axios.get(`http://localhost:3001/services`)
+      setServices(response.data)
+    }
+    getServices()
+  }, [])
+
   return (
     <div>
-      <h1>Home</h1>
+      <div className="home-article-container">
+        {articles
+          ? articles.map((article) => (
+              <div>
+                <img src={article.picture1} width="300" />
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="home-market-container">
+        {markets
+          ? markets.map((market) => (
+              <div>
+                <img src={market.picture1} width="400" />
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="home-art-container">
+        {art
+          ? art.map((artPiece) => (
+              <div>
+                <img src={artPiece.picture1} width="500" />
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="home-services-container">
+        {services
+          ? services.map((service) => (
+              <div>
+                <img src={service.picture1} width="600" />
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   )
 }
